9e9681e3afbdb21c2c815beeec368e208b49ca5c
Silavsale <silavsale@gmail.com>
2024-11-22 12:50:57 -0500

    Add Sentry

--- src/index.js
+++ src/index.js
@@ -2,7 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+
+const sentryDsn = process.env.REACT_APP_SENTRY_DSN;
+
+Sentry.init({
+  dsn: sentryDsn,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({maskAllText: false,
+      blockAllMedia: false,}),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +31,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
